49d561e44a8162ba4f848a2dd2a55da877b5d28d
Hamed Nazari <hamed.nazari70@gmail.com>
2022-12-30 01:03:11 -0600

    add Setting file

---
+++ setting.py
@@ -0,0 +1,18 @@
+DISCOVERY_PERIOD = 10# #5For discovering topology.
+
+MONITOR_PERIOD = 10# #10For monitoring traffic
+
+DELAY_DETECTING_PERIOD = 8 #8
+
+TOSHOW = False	   # For showing information in terminal
+
+# PATH_TO_FILES = "/home/csnetuofr/monitoring/TM.txt"
+
+#FOR DRSIR
+PATH_TO_FILES = "/home/csnetuofr/monitoring"
+
+NUMBER_OF_NODES = 32
+
+'''
+para 64 nodos intente correr con discover 10, monitor 15, delay 13  pero cuparece que el monitor no es suficiente para el drl, si toca aumenta rmuco pues paila, entones savoy a ahcer con 48nodos
+'''
