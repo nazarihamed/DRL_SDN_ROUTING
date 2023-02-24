2ca2e2e5179e1a3b2c481241a274103f484cfa8a
Hamed Nazari <hamed.nazari70@gmail.com>
2023-02-24 01:06:45 -0600

    Adding DRL

---
+++ SDNapps_proac/Config.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class Thread(Enum):
+    Nodes_23 = 28
+    Nodes_32 = 40
+    Nodes_48 = 38
+    Nodes_64 = 45
+
+
+
+Environment = "environment_test_32nodes"
+drl_thread = Thread.Nodes_23
+num_nodes = 23
